7e772836c64be82a0478eaf4df66bd2aa4134015
gala1234 <calero.vg@gmail.com>
2021-10-06 16:14:40 +0200

    revert populate changes

--- api/category/controllers/category.js
+++ api/category/controllers/category.js
@@ -1,21 +1,10 @@
 'use strict';
 
-const { sanitizeEntity } = require('strapi-utils');
-
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/concepts/controllers.html#core-controllers)
  * to customize this controller
  */
 
 module.exports = {
-  async find(ctx) {
-    let entities;
-    if (ctx.query._q) {
-      entities = await strapi.services.category.search(ctx.query, []);
-    } else {
-      entities = await strapi.services.category.find(ctx.query, []);
-    }
-    return entities.map((entity) => sanitizeEntity(entity, { model: strapi.models.category }))
-  },
 };
 
--- api/static-page/controllers/static-page.js
+++ api/static-page/controllers/static-page.js
@@ -1,20 +1,9 @@
 'use strict';
 
-const { sanitizeEntity } = require('strapi-utils');
-
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/concepts/controllers.html#core-controllers)
  * to customize this controller
  */
 
 module.exports = {
-    async find(ctx) {
-        let entities;
-        if (ctx.query._q) {
-            entities = await strapi.services.static - page.search(ctx.query, []);
-        } else {
-            entities = await strapi.services.static - page.find(ctx.query, []);
-        }
-        return entities.map((entity) => sanitizeEntity(entity, { model: strapi.models.static - page }))
-    },
 };
