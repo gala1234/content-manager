4683f4aad6ea864df382b189eb0635bf77cb150e
Gala <calero.vg@gmail.com>
2023-01-02 12:59:06 +0100

    trigger build

--- config/plugins.js
+++ config/plugins.js
@@ -17,6 +17,16 @@ module.exports = ({ env }) => {
     }
 
     return {
-        //Empty config which will use defaults
+        upload: {
+            provider: 'aws-s3',
+            providerOptions: {
+                accessKeyId: env('AWS_ACCESS_KEY_ID'),
+                secretAccessKey: env('AWS_ACCESS_SECRET'),
+                region: env('AWS_REGION'),
+                params: {
+                    Bucket: env('AWS_BUCKET'),
+                },
+            },
+        },
     }
 }
